425f37e6a821f5bcfb347f4868e6a0ae771b8bc4
杨柳 <1545766219@qq.com>
2017-04-19 16:57:18 +0800

    引入jquery

--- app/index.js
+++ app/index.js
@@ -2,13 +2,14 @@
  * Created by yangliu on 2017/4/19.
  */
 import _ from 'lodash';
+import j from 'jquery'
 function component () {
-    var element = document.createElement('div');
+    var element = j('<div></div>');
 
     /* lodash is required for the next line to work */
-    element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-    return element;
+    element.html(_.join(['Hello','webpack'], ' '))
+    
+    return element.get(0);
 }
 
 document.body.appendChild(component());
